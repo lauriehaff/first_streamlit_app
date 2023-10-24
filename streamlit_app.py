1e1613fb902ad22dccf91060e34e718d7014d8b7
lauriehaff <148783743+lauriehaff@users.noreply.github.com>
2023-10-24 15:41:34 +0200

    Update streamlit_app.py

--- streamlit_app.py
+++ streamlit_app.py
@@ -1,4 +1,11 @@
 import streamlit
 
-streamlit.title ("This is my first app without paillettes")
+streamlit.title ("My mom's new healthy diner")
+
+streamlit.header('Breakfast Menu')
+streamlit.text(' 🥣 Omega 3 & Blueberry Oatmeal')
+streamlit.text('🥗 Kale, Spinach & Rocket Smoothie')
+streamlit.text('🐔 Hard-Boiled Free-Range Egg')
+streamlit.text('🥑🍞Avocado Toast')
+
 
